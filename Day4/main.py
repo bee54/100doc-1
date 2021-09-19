d62274e290849d91f1ca954f6ff295ff3d2f7926
thisrepl <86901453+bee54@users.noreply.github.com>
2021-09-19 20:46:52 +0000

    Day 4 Skipped Day 3 because I didn't want to write another map-list text game... not right now anyway. Will change Day projects main python to be main.py in all cases to make it easier to change the Run file in the .replit  ... there must be a better way to switch between a lot of repls

---
+++ Day4/main.py
@@ -0,0 +1,14 @@
+row1=["■","■","■"]
+row2=["■","■","■"]
+row3=["■","■","■"]
+
+
+map= [row1,row2,row3]
+print(f"{row1}\n{row2}\n{row3}\n")
+
+position=input("Type two ints: (50)")
+
+row=int(position[0])
+col=int(position[1])
+
+
