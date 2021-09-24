1051a634b9045f89b5aa69a8e1c0dbdaea25d28e
thisrepl <86901453+bee54@users.noreply.github.com>
2021-09-24 02:36:37 +0000

    Day 4 more Tried to use a "smart" approach to the winning decision and its ending up to be hard to look at.

--- Day4/main.py
+++ Day4/main.py
@@ -3,48 +3,38 @@ import random
 
 
 #Rock Paper Scissors. One round
-rounds=int(input("How many Rock Paper and Scissors can you take?"))
+rounds=int(input("How many games of Rock Paper and Scissors can you take?"))
 #^^ It turns out int can't take a decimal so its just typing without any boxing
 
 
 
 def getUserInputs():
-  return input("Type 0 for rock, 1 for paper, 2 for scissors:\nUser choice: ") # game string 1
+  return input("Type 0 for rock, 1 for paper, 2 for scissors:")
 
 
 
 def runGame():
 
-  userChoice=int(getUserInputs())
+  playerChoice=int(getUserInputs())
   computerChoice=random.randint(0, 2)
 
+  # Print inputs
+  print(f"Computer: {computerChoice}                 {playerChoice}: User")
 
-  print(f"Computer choice: {computerChoice}")
+
+  #Winner decider
+
+  if computerChoice == playerChoice: # If its a tie
+    print("Tie")
+  elif computerChoice > playerChoice or computerChoice % 2 == playerChoice:
+    print("Computer WINS")
+  elif computerChoice > playerChoice or computerChoice  == playerChoice % 2:
+    print("Player WINS")
 
 
-  if computerChoice == userChoice:
-    print("Two losers don't make a winner.")
-  elif computerChoice < userChoice:
-    print("#################")
-    print("#               #")
-    print("# Computer wins #")
-    print("#               #")
-    print("#################")
-    #print("computerChoice  < userChoice")
-  elif computerChoice < userChoice-1:
-    print("#################")
-    print("#               #")
-    print("# Computer wins #")
-    print("#               #")
-    print("#################")
-    #print("computerChoice  < userChoice")
-  elif computerChoice > userChoice:
-    print("#################")
-    print("#               #")
-    print("# User     wins #")
-    print("#               #")
-    print("#################")
-    #print("computerChoice  > userChoice, hopefully")
+
+
+
 
 
 
